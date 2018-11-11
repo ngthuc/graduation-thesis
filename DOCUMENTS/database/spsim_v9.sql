b7775775bd4ba3a641238596c13e231741e9f214
Nguyen Thuc <lenguyenthuc.edu@gmail.com>
2018-11-11 12:00:51 +0700

    Update DOCUMENTS/database/spsim_v9.sql

---
+++ DOCUMENTS/database/spsim_v9.sql
@@ -0,0 +1,292 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.9
+-- https://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Nov 11, 2018 at 01:47 AM
+-- Server version: 5.6.37
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `luanvan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ARTICLE`
+--
+
+CREATE TABLE IF NOT EXISTS `ARTICLE` (
+  `ARTICLEID` int(5) NOT NULL,
+  `USERID` varchar(30) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
+  `CATEID` int(11) NOT NULL,
+  `ARTICLEIMAGE` text COLLATE utf8mb4_vietnamese_ci,
+  `ARTICLETITLE` text COLLATE utf8mb4_vietnamese_ci,
+  `ARTICLEDESCRIPTION` text COLLATE utf8mb4_vietnamese_ci,
+  `ARTICLECONTENT` longtext COLLATE utf8mb4_vietnamese_ci,
+  `ARTICLECREATIONDATE` datetime DEFAULT NULL,
+  `ARTICLECOUNT` int(20) DEFAULT NULL,
+  `ARTICLEPOLICY` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `ARTICLETYPE` text COLLATE utf8mb4_vietnamese_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `CATEGORY`
+--
+
+CREATE TABLE IF NOT EXISTS `CATEGORY` (
+  `CATEID` int(5) NOT NULL,
+  `USERID` varchar(30) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `CAT_CATEID` int(5) DEFAULT NULL,
+  `CATENAME` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `CATELEVEL` int(11) DEFAULT NULL,
+  `CATESHOWMENU` tinyint(1) DEFAULT NULL,
+  `CATEPOLICY` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `CATETYPE` text COLLATE utf8mb4_vietnamese_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `INFO`
+--
+
+CREATE TABLE IF NOT EXISTS `INFO` (
+  `INFOID` int(5) NOT NULL,
+  `USERID` varchar(30) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
+  `CATEID` int(11) DEFAULT NULL,
+  `INFOIMAGE` text COLLATE utf8mb4_vietnamese_ci,
+  `INFODATE` text COLLATE utf8mb4_vietnamese_ci,
+  `INFOTITLE` text COLLATE utf8mb4_vietnamese_ci,
+  `INFODESCRIPTION` text COLLATE utf8mb4_vietnamese_ci,
+  `INFOCONTENT` longtext COLLATE utf8mb4_vietnamese_ci,
+  `INFOPOLICY` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `INFOTYPE` text COLLATE utf8mb4_vietnamese_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+--
+-- Dumping data for table `INFO`
+--
+
+INSERT INTO `INFO` (`INFOID`, `USERID`, `CATEID`, `INFOIMAGE`, `INFODATE`, `INFOTITLE`, `INFODESCRIPTION`, `INFOCONTENT`, `INFOPOLICY`, `INFOTYPE`) VALUES
+(1, 'ngthuc', NULL, NULL, NULL, 'name', NULL, 'THANH-NGHI DO', 'public', 'person'),
+(2, 'ngthuc', NULL, NULL, '1974-02-12', 'birthday', NULL, '', 'public', 'person'),
+(3, 'ngthuc', NULL, NULL, NULL, 'gender', NULL, 'Male', 'public', 'person'),
+(4, 'ngthuc', NULL, NULL, NULL, 'email', NULL, 'dtnghi@cit.ctu.edu.vn', 'public', 'person'),
+(5, 'ngthuc', NULL, NULL, NULL, 'phone', NULL, '+84 (0)2923 734 720', 'public', 'person'),
+(6, 'ngthuc', NULL, NULL, NULL, 'address', NULL, '3/2 Street, Ninh Kieu District, 92100-CanTho, Viet Nam', 'public', 'person'),
+(7, 'ngthuc', NULL, NULL, NULL, 'website', NULL, 'http://www.cit.ctu.edu.vn/~dtnghi/', 'public', 'person'),
+(8, 'ngthuc', NULL, 'http://cit.ctu.edu.vn/~dtnghi/Nghi-CV1_files/getPhoto.jpg', NULL, 'avatar', NULL, '', 'public', 'person'),
+(9, 'ngthuc', NULL, NULL, NULL, 'position', NULL, 'Head of Computer Networks Department', 'public', 'person');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `MEDIA`
+--
+
+CREATE TABLE IF NOT EXISTS `MEDIA` (
+  `MEDIAID` int(11) NOT NULL,
+  `CATEID` int(11) DEFAULT NULL,
+  `USERID` varchar(30) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
+  `MEDIATITLE` text COLLATE utf8mb4_vietnamese_ci,
+  `MEDIADATA` text COLLATE utf8mb4_vietnamese_ci,
+  `MEDIALINK` text COLLATE utf8mb4_vietnamese_ci,
+  `MEDIAPOLICY` varchar(100) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
+  `MEDIATYPE` varchar(100) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `MENU`
+--
+
+CREATE TABLE IF NOT EXISTS `MENU` (
+  `MENUID` int(5) NOT NULL,
+  `USERID` varchar(30) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `MENUPARENT` int(5) DEFAULT NULL,
+  `MENUNAME` text COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `MENULEVEL` int(11) DEFAULT NULL,
+  `MENUPOSITION` int(11) DEFAULT '1',
+  `MENUTYPE` text COLLATE utf8mb4_vietnamese_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `SYSTEM`
+--
+
+CREATE TABLE IF NOT EXISTS `SYSTEM` (
+  `SYSTEMID` int(11) NOT NULL,
+  `SYSTEMTITLE` text COLLATE utf8mb4_vietnamese_ci,
+  `SYSTEMDATA` text COLLATE utf8mb4_vietnamese_ci,
+  `SYSTEMLINK` text COLLATE utf8mb4_vietnamese_ci,
+  `SYSTEMPOLICY` varchar(100) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
+  `SYSTEMTYPE` varchar(100) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `USERS`
+--
+
+CREATE TABLE IF NOT EXISTS `USERS` (
+  `USERID` varchar(30) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `USERFULLNAME` varchar(254) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `USEREMAIL` varchar(100) COLLATE utf8mb4_vietnamese_ci NOT NULL,
+  `USERPASSWORD` varchar(50) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
+  `USERROLE` varchar(255) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL,
+  `USERSTATUS` char(10) COLLATE utf8mb4_vietnamese_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+--
+-- Dumping data for table `USERS`
+--
+
+INSERT INTO `USERS` (`USERID`, `USERFULLNAME`, `USEREMAIL`, `USERPASSWORD`, `USERROLE`, `USERSTATUS`) VALUES
+('ngthuc', 'Nguyên Thức', 'ngthuc@lapvo3.tk', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'approved'),
+('ngthuc.hrm', 'HRM Nguyen Thuc', 'ngthuc.hrm@lapvo3.tk', NULL, 'user', 'pending');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ARTICLE`
+--
+ALTER TABLE `ARTICLE`
+  ADD PRIMARY KEY (`ARTICLEID`),
+  ADD KEY `FK_ARTICLECATEGORY` (`CATEID`),
+  ADD KEY `FK_ARTICLEUSERS` (`USERID`);
+
+--
+-- Indexes for table `CATEGORY`
+--
+ALTER TABLE `CATEGORY`
+  ADD PRIMARY KEY (`CATEID`),
+  ADD KEY `FK_CATEGORYUSERS` (`USERID`);
+
+--
+-- Indexes for table `INFO`
+--
+ALTER TABLE `INFO`
+  ADD PRIMARY KEY (`INFOID`),
+  ADD KEY `FK_INFOCATEGORY` (`CATEID`),
+  ADD KEY `FK_INFOUSERS` (`USERID`);
+
+--
+-- Indexes for table `MEDIA`
+--
+ALTER TABLE `MEDIA`
+  ADD PRIMARY KEY (`MEDIAID`),
+  ADD KEY `FK_MEDIACATEGORY` (`CATEID`),
+  ADD KEY `FK_MEDIAUSERS` (`USERID`);
+
+--
+-- Indexes for table `MENU`
+--
+ALTER TABLE `MENU`
+  ADD PRIMARY KEY (`MENUID`),
+  ADD KEY `FK_MENUUSERS` (`USERID`);
+
+--
+-- Indexes for table `SYSTEM`
+--
+ALTER TABLE `SYSTEM`
+  ADD PRIMARY KEY (`SYSTEMID`);
+
+--
+-- Indexes for table `USERS`
+--
+ALTER TABLE `USERS`
+  ADD PRIMARY KEY (`USERID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ARTICLE`
+--
+ALTER TABLE `ARTICLE`
+  MODIFY `ARTICLEID` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `CATEGORY`
+--
+ALTER TABLE `CATEGORY`
+  MODIFY `CATEID` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `INFO`
+--
+ALTER TABLE `INFO`
+  MODIFY `INFOID` int(5) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `MEDIA`
+--
+ALTER TABLE `MEDIA`
+  MODIFY `MEDIAID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `MENU`
+--
+ALTER TABLE `MENU`
+  MODIFY `MENUID` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `SYSTEM`
+--
+ALTER TABLE `SYSTEM`
+  MODIFY `SYSTEMID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `ARTICLE`
+--
+ALTER TABLE `ARTICLE`
+  ADD CONSTRAINT `FK_ARTICLECATEGORY` FOREIGN KEY (`CATEID`) REFERENCES `CATEGORY` (`CATEID`),
+  ADD CONSTRAINT `FK_ARTICLEUSERS` FOREIGN KEY (`USERID`) REFERENCES `USERS` (`USERID`);
+
+--
+-- Constraints for table `CATEGORY`
+--
+ALTER TABLE `CATEGORY`
+  ADD CONSTRAINT `FK_CATEGORYUSERS` FOREIGN KEY (`USERID`) REFERENCES `USERS` (`USERID`);
+
+--
+-- Constraints for table `INFO`
+--
+ALTER TABLE `INFO`
+  ADD CONSTRAINT `FK_INFOCATEGORY` FOREIGN KEY (`CATEID`) REFERENCES `CATEGORY` (`CATEID`),
+  ADD CONSTRAINT `FK_INFOUSERS` FOREIGN KEY (`USERID`) REFERENCES `USERS` (`USERID`);
+
+--
+-- Constraints for table `MEDIA`
+--
+ALTER TABLE `MEDIA`
+  ADD CONSTRAINT `FK_MEDIACATEGORY` FOREIGN KEY (`CATEID`) REFERENCES `CATEGORY` (`CATEID`),
+  ADD CONSTRAINT `FK_MEDIAUSERS` FOREIGN KEY (`USERID`) REFERENCES `USERS` (`USERID`);
+
+--
+-- Constraints for table `MENU`
+--
+ALTER TABLE `MENU`
+  ADD CONSTRAINT `FK_MENUUSERS` FOREIGN KEY (`USERID`) REFERENCES `USERS` (`USERID`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
